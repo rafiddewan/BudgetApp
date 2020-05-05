d5cb7f46664a6334949c9e2de246d87b59eb4da7
rafiddewan <rafid.dewan145@gmail.com>
2020-05-05 00:23:59 -0400

    set up MVC

---
+++ app.js
@@ -0,0 +1,21 @@
+/*************
+ * Modules will help keep pieces of code together inside independent organized units
+ * Module pattern will create modules for us
+ */
+
+ //Model Module
+ var budgetModel = (function() {
+    
+ })();
+
+ //UI module
+var budgetView = (function(){
+    
+
+})();
+
+
+//Controller Module
+var budgetController = (function(model, view){
+    
+})(budgetModel, budgetView);
